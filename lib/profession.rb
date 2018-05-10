88881d7aced2b37020899bf86d037ed395225802
Nick Gorbikoff <nick.gorbikoff@gmail.com>
2018-05-10 18:42:03 -0500

    Adding tests and simple functionality

--- lib/profession.rb
+++ lib/profession.rb
@@ -2,23 +2,46 @@ require 'profession/version'
 require 'yaml'
 
 module Profession
-  PROFESSIONS_YML = YAML.load_file(File.join(File.dirname(__FILE__), 'profession', 'profession.yml'))
-  PROFESSIONS     = PROFESSION_YML.map { |_, v| v }.flatten.compact.sort
+  PROFESSIONS_YML       = YAML.load_file(File.join(File.dirname(__FILE__), 'profession', 'professions.yml'))
+  PROFESSIONS           = PROFESSIONS_YML.map { |_, v| v }.flatten.compact.sort
+  PROFESSIONS_DOWNCASED = PROFESSIONS.map(&:downcase)
 
   class Profession
     class << self
       def size
-        PROFESSION.map { |v| v }.flatten.size
+        PROFESSIONS.map { |v| v }.flatten.size
       end
 
-      def includes?(option)
-        PROFESSIONS.include? clean_profession(option)
+      def list
+        PROFESSIONS
+      end
+
+      def contains_exact?(option)
+        PROFESSIONS_DOWNCASED.include? clean_downcase_profession(option)
+      end
+
+      def exact_match(option)
+        PROFESSIONS_DOWNCASED.
+          select { |x| x == clean_downcase_profession(option) }.
+          map do |str|
+          str.split(' ').
+            map(&:capitalize).join(' ')
+        end
+      end
+
+      def match(option)
+        PROFESSIONS_DOWNCASED.
+          select { |x| x.include? clean_downcase_profession(option) }.
+          map do |str|
+          str.split(' ').
+            map(&:capitalize).join(' ')
+        end
       end
 
       private
 
-        def clean_profession(option)
-          option.to_s.strip.squish.downcase
+        def clean_downcase_profession(option)
+          option.to_s.strip.split(' ').map(&:strip).join(' ').downcase
         end
     end
   end
--- test/profession_test.rb
+++ test/profession_test.rb
@@ -5,7 +5,29 @@ class ProfessionTest < Minitest::Test
     refute_nil ::Profession::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_professions_are_loaded
+    assert (Profession::Profession.size == 1152)
   end
+
+  def test_professions_is_an_array_of_string_professions
+    assert Profession::Profession.list[15].is_a? String
+  end
+
+  def test_contains_exact_match?
+    assert Profession::Profession.contains_exact?('Zoologist')
+    refute Profession::Profession.contains_exact?('Clamper')
+    refute Profession::Profession.contains_exact?('Storm Trooper')
+  end
+
+  def test_exact_match
+    assert ['Zoo Keeper'] == Profession::Profession.exact_match('Zoo Keeper')
+    assert [] == Profession::Profession.exact_match('Keeper')
+  end
+
+  def test_match
+    refute_empty Profession::Profession.match('Zoo Keeper')
+    refute_empty Profession::Profession.match('Keeper')
+    assert Profession::Profession.match('Keeper').size > 1
+  end
+
 end
